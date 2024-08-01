10596ef752e19db9519f5f0844a9c364c0eb16e0
Bagdadyan_G <lpsneka@email.com>
2024-08-01 16:39:49 +0300

    hw 1.5/3

--- src/main2.ts
+++ src/main2.ts
@@ -25,98 +25,105 @@ let totalPages = 1
 const moviesPerPage = 3
 const maxPageButtons = 5
 
-let filmList: any[] = [{ "Title": "Teenage Mutant Ninja Turtles", "Year": "2014", "Rated": "PG-13", "Released": "08 Aug 2014", "Runtime": "101 min", "Genre": "Action, Adventure, Comedy", "Director": "Jonathan Liebesman", "Writer": "Josh Appelbaum, André Nemec, Evan Daugherty", "Actors": "Megan Fox, Will Arnett, William Fichtner", "Plot": "When a kingpin threatens New York City, a group of mutated turtle warriors must emerge from the shadows to protect their home.", "Language": "English, Japanese", "Country": "United States", "Awards": "1 win & 11 nominations", "Poster": "https://m.media-amazon.com/images/M/MV5BNjUzODQ5MDY5NV5BMl5BanBnXkFtZTgwOTc1NzcyMjE@._V1_SX300.jpg", "Ratings": [{ "Source": "Internet Movie Database", "Value": "5.8/10" }, { "Source": "Rotten Tomatoes", "Value": "21%" }, { "Source": "Metacritic", "Value": "31/100" }], "Metascore": "31", "imdbRating": "5.8", "imdbVotes": "221,385", "imdbID": "tt1291150", "Type": "movie", "DVD": "N/A", "BoxOffice": "$191,204,754", "Production": "N/A", "Website": "N/A", "Response": "True" }]
+let filmList: any[] = []
 
-// searchBtn.addEventListener('click', (e) => {
-//     e.preventDefault()
-//     activePage = 1
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    activePage = 1
 
-//     axios(`http://www.omdbapi.com/?apikey=c07de1fa&s=${inputTitle.value}&type=${selectType.value}`)
-//         .then((resp) => {
-//             console.log(resp.data.Response)
-//             if (resp.data.Response == 'False') {
-//                 let errorMovie = document.createElement('div')
-//                 errorMovie.textContent = 'Movie not found!'
-//                 errorMovie.id = 'errorMovie'
-//                 document.body.appendChild(errorMovie)
-//             } else if (resp.data.Response == 'True') {
-//                 panelResult.hidden = false
-//                 filmList = resp.data.Search
-//                 totalPages = Math.ceil(filmList.length / moviesPerPage)
+    axios(`http://www.omdbapi.com/?apikey=c07de1fa&s=${inputTitle.value}&type=${selectType.value}`)
+        .then((resp) => {
+            console.log(resp.data.Response)
+            if (resp.data.Response == 'False') {
+                let errorMovie = document.createElement('div')
+                errorMovie.textContent = 'Movie not found!'
+                errorMovie.id = 'errorMovie'
+                document.body.appendChild(errorMovie)
+            } else if (resp.data.Response == 'True') {
+                panelResult.hidden = false
+                filmList = resp.data.Search    
+                // filmList.push(resp.data.Search)
+                totalPages = Math.ceil(filmList.length / moviesPerPage)
 
-//                 createPaginationButtons(totalPages)
+                addToList(filmList)
+                createPaginationButtons(totalPages)
 
-//                 displayMovies()
+                displayMovies()
 
-//                 nextBtn.addEventListener('click', () => {
-//                     if (activePage < totalPages) {
-//                         activePage++
-//                         displayMovies()
-//                     }
+                nextBtn.addEventListener('click', () => {
+                    if (activePage < totalPages) {
+                        activePage++
+                        displayMovies()
+                    }
 
-//                 })
-//                 prevBtn.addEventListener('click', () => {
-//                     if (activePage > 1) {
-//                         activePage--
-//                         displayMovies()
-//                     }
-//                 })
+                })
+                prevBtn.addEventListener('click', () => {
+                    if (activePage > 1) {
+                        activePage--
+                        displayMovies()
+                    }
+                })
 
-//             }
-//         })
-// })
-// function createPaginationButtons(totalPages: number) {
-//     pageNumbers.innerHTML = ''
-//     for (let i = 0; i < Math.min(totalPages, maxPageButtons); i++) {
-//         let btn = document.createElement('button')
-//         btn.textContent = `${i + 1}`
-//         btn.classList.add('buttonResult')
-//         pageNumbers.appendChild(btn)
-//         btn.addEventListener('click', () => {
-//             activePage = i + 1
-//             displayMovies()
-//         })
-//     }
-// }
+            }
+        })
+})
+function createPaginationButtons(totalPages: number) {
+    pageNumbers.innerHTML = ''
+    for (let i = 0; i < Math.min(totalPages, maxPageButtons); i++) {
+        let btn = document.createElement('button')
+        btn.textContent = `${i + 1}`
+        btn.classList.add('buttonResult')
+        pageNumbers.appendChild(btn)
+        btn.addEventListener('click', () => {
+            activePage = i + 1
+            displayMovies()
+        })
+    }
+}
 
-// function updatePaginationButtons() {
-//     // Math.floor(maxPageButtons / 2 всегда 2
-//     const startPage = 1
-//     const endPage = Math.min(totalPages, startPage + maxPageButtons - 1)
+function addToList(movie:any){
+    for(let i = 0;i < filmList.length;i++){
+        console.log(filmList[i])
+    }
+}
+function updatePaginationButtons() {
+    // Math.floor(maxPageButtons / 2 всегда 2
+    const startPage = 1
+    const endPage = Math.min(totalPages, startPage + maxPageButtons - 1)
 
-//     pageNumbers.innerHTML = ''
+    pageNumbers.innerHTML = ''
 
-//     for (let i = startPage; i <= endPage; i++) {
-//         let btn = document.createElement('button')
-//         btn.textContent = `${i}`
-//         btn.classList.add('buttonResult')
-//         pageNumbers.appendChild(btn)
+    for (let i = startPage; i <= endPage; i++) {
+        let btn = document.createElement('button')
+        btn.textContent = `${i}`
+        btn.classList.add('buttonResult')
+        pageNumbers.appendChild(btn)
 
-//         btn.addEventListener('click', () => {
-//             activePage = i
-//             displayMovies()
-//         })
-//     }
-// }
+        btn.addEventListener('click', () => {
+            activePage = i
+            displayMovies()
+        })
+    }
+}
 
-// function displayMovies() {
-//     const startIndex = (activePage - 1) * moviesPerPage
-//     const endIndex = startIndex + moviesPerPage
-//     const moviesToShow = filmList.slice(startIndex, endIndex)
+function displayMovies() {
+    const startIndex = (activePage - 1) * moviesPerPage
+    const endIndex = startIndex + moviesPerPage
+    const moviesToShow = filmList.slice(startIndex, endIndex)
 
-//     for (let i = 0; i < moviesPerPage; i++) {
-//         if (i < moviesToShow.length) {
-//             imgPoster[i].src = moviesToShow[i].Poster
-//             typeName[i].textContent = selectType.value
-//             filmName[i].textContent = moviesToShow[i].Title
-//             yearName[i].textContent = moviesToShow[i].Year
-//             movieElements[i].style.display = 'flex'
-//         } else {
-//             movieElements[i].style.display = 'none'
-//         }
-//     }
-//     updatePaginationButtons()
-// }
+    for (let i = 0; i < moviesPerPage; i++) {
+        if (i < moviesToShow.length) {
+            imgPoster[i].src = moviesToShow[i].Poster
+            typeName[i].textContent = selectType.value
+            filmName[i].textContent = moviesToShow[i].Title
+            yearName[i].textContent = moviesToShow[i].Year
+            movieElements[i].style.display = 'flex'
+        } else {
+            movieElements[i].style.display = 'none'
+        }
+    }
+    updatePaginationButtons()
+}
 
 
 let modalContainer = document.querySelector('.modalContainer') as HTMLDivElement
@@ -129,25 +136,24 @@ let director = document.querySelector('.Director') as HTMLParagraphElement
 let writer = document.querySelector('.Writer') as HTMLParagraphElement
 let actors = document.querySelector('.Actors') as HTMLParagraphElement
 let awards = document.querySelector('.Awards') as HTMLParagraphElement
+let mainMovieImg = document.querySelector('.mainMovieImg') as HTMLImageElement
 
 document.addEventListener('click', (e) => {
     let target = e.target as HTMLElement
-    let btn = target.closest('.detailsBtn')
-    let movie = target.closest('.filmName')?.textContent
+    let btn = target.closest('.detailsBtn') as HTMLButtonElement
+    if (!btn) return false
+    let movie = btn.closest('.detailsBtn')?.parentElement?.querySelector('.filmName')?.textContent
+    console.log(movie)
     let realList: any[] = []
-    if (!btn) return
     for (let el = 0; el < filmList.length; el++) {
         if (movie == filmList[el].Title) {
             console.log(filmList[el])
             realList.push(filmList[el])
         }
     }
-    console.log(realList)
-    console.log(movie)
-    console.log(filmList)
+    displayMainFilmInfo(realList[0])
     modalContainer.style.opacity = '1'
     modalContainer.style.pointerEvents = 'all'
-    displayMainFilmInfo(realList)
 })
 closeBtn.addEventListener('click', () => {
     modalContainer.style.opacity = '0'
@@ -155,6 +161,8 @@ closeBtn.addEventListener('click', () => {
 })
 
 function displayMainFilmInfo(movie: any) {
+    console.log(movie.Title, movie.Released, movie.Genre, movie.Country, movie.Director, movie.Writer, movie.Actors, movie.Awards)
+    mainMovieImg.src = movie.Poster
     title.textContent = movie.Title
     released.textContent = movie.Released
     genre.textContent = movie.Genre
